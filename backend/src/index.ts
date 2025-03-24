f9c9e9f8425dbaba1184dd7a073128584b6df12c
tuzicomet <leo336570@gmail.com>
2025-03-25 02:28:22 +1300

    index.ts now uses port from .env instead of a set one
    Using dotenv to get values from the .env. Defaults to 5000 if not provided

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
-// NOTE: although the actual file is .ts, it will be compiled into .js, so we need to use that
+import dotenv from "dotenv";
+// NOTE: although the actual file is .ts, it'll compile to .js, which we must use
 import authRoutes from "./routes/auth.route.js";
+
+// Load environment variables from the .env file
+dotenv.config();
 
 // Create instance of an Express app
 const app = express();
-// Set the port which the server will listen/run on
-const PORT = 5000;
+// Get the port which the server will run on from the .env file, default to 5000 if not set
+const PORT = process.env.PORT || 5000;
 
 // Any request to /api/auth/... will be handled by authRoutes
 app.use("/api/auth", authRoutes);
