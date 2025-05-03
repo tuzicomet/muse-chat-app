6f72225b3b77c0d3aff5dcc7924e21e46de9d8d9
tuzicomet <leo336570@gmail.com>
2025-05-04 01:17:58 +1200

    Added a /update-profile route
    uses protectRoute so that users must be authenticated to access it

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js"
+import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 // Express router instance
 const router = express.Router();
@@ -12,6 +13,11 @@ router.post("/login", login);
 
 // Logout route
 router.post("/logout", logout);
+
+// All routes below require authentication via the protectRoute middleware.
+
+// Route for updating profile
+router.put("/update-profile", protectRoute, updateProfile);
 
 // Export router to be usable in other files
 export default router;
