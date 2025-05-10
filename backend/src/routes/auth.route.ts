c4c353651ea919c7d07f40e979b248709bf2dbf4
tuzicomet <leo336570@gmail.com>
2025-05-10 22:11:58 +1200

    /check route added
    This route checks if the user is authenticated. To be used primarily on frontend app load or refresh to verify the user's session.

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
+import { signup, login, logout, checkAuth, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 // Express router instance
@@ -15,6 +15,9 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 // All routes below require authentication via the protectRoute middleware.
+
+// Route for checking authentication
+router.get("/check", protectRoute, checkAuth);
 
 // Route for updating profile
 router.put("/update-profile", protectRoute, updateProfile);
