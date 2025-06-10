69acfa41e4af02259f105c837fa456c4a1101dfb
tuzicomet <leo336570@gmail.com>
2025-06-10 23:50:41 +1200

    React app can now use components from react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* Wrap the App with BrowserRouter so it can use React Router components */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
