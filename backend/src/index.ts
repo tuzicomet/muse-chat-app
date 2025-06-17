cec0f717680c6eaab31f666bea5aff549c0003f7
tuzicomet <leo336570@gmail.com>
2025-06-17 23:35:14 +1200

    Added cors middleware to allow requests from frontend with credentials

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // NOTE: when importing .ts files, they'll compile to .js, so we must use the .js extension
 import { connectDB } from "./lib/db.js";
@@ -20,6 +21,13 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 // Middleware to parse cookies, so that we can extract their values
 app.use(cookieParser());
+// Middleware to enable Cross-Origin Resource Sharing (CORS)
+app.use(cors({
+  // Allow requests from http://localhost:3000 (frontend server)
+  origin: "http://localhost:3000",
+  // allow cookies and auth headers to be sent with requests
+  credentials: true
+}));
 
 // Any request to /api/auth/... will be handled by authRoutes
 app.use("/api/auth", authRoutes);
